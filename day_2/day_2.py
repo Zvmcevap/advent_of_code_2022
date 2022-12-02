b52bb829ee5300ac26865f1393ef5e927a328be3
Zvmcevap <zupanc.beno@gmail.com>
2022-12-02 14:11:21 +0100

    Day 2 done and done

---
+++ day_2/day_2.py
@@ -0,0 +1,83 @@
+opponent = []
+me = []
+
+with open("day_2.txt") as f:
+    for line in f.readlines():
+        opponent.append(line.strip()[0])
+        me.append(line.strip()[2])
+
+hand_points = {
+    "Rock": 1,
+    "Paper": 2,
+    "Scissors": 3
+}
+end_points = {
+    "Win": 6,
+    "Draw": 3,
+    "Loss": 0
+}
+opps_hand = {
+    "A": "Rock",
+    "B": "Paper",
+    "C": "Scissors"
+}
+
+def part_one():
+    my_hand = {
+        "X": "Rock",
+        "Y": "Paper",
+        "Z": "Scissors"
+    }
+    score = 0
+    for i in range(len(opponent)):
+        opp_round = opps_hand[opponent[i]]
+        my_round = my_hand[me[i]]
+        score += hand_points[my_round]
+
+        if opp_round == my_round:
+            score += 3
+        elif (opp_round == "Rock" and my_round == "Paper") or (opp_round == "Paper" and my_round == "Scissors") or (opp_round == "Scissors" and my_round == "Rock"):
+            score += 6
+
+    print(f"Part One Score = {score}")
+
+
+def part_two():
+    my_hand = {
+        "X": "Loss",
+        "Y": "Draw",
+        "Z": "Win"
+    }
+    score = 0
+    for i in range(len(opponent)):
+        opp_round = opps_hand[opponent[i]]
+        round_end = my_hand[me[i]]
+
+        score += end_points[round_end]
+
+        if round_end == "Draw":
+            my_round = opp_round
+
+        elif round_end == "Win":
+            if opp_round == "Rock":
+                my_round = "Paper"
+            elif opp_round == "Paper":
+                my_round = "Scissors"
+            else:
+                my_round = "Rock"
+        else:
+            if opp_round == "Rock":
+                my_round = "Scissors"
+            elif opp_round == "Paper":
+                my_round = "Rock"
+            else:
+                my_round = "Paper"
+
+        score += hand_points[my_round]
+
+    print(f"Part Two Score = {score}")
+
+
+if __name__ == "__main__":
+    part_one()
+    part_two()
